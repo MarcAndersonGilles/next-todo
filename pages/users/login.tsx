86c9b3e9cc1cca8a65691c4ae58451b8e7a2b81d
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-04-18 16:51:46 -0400

    package supabase for login

---
+++ pages/users/login.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { SupabaseClient } from '@supabase/supabase-js'
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { useSession  } from '@supabase/auth-helpers-react'
+import { createClient } from '@supabase/supabase-js'
+import { useRouter } from 'next/router';
+function login() {
+  
+  return (
+    <div>login</div>
+  )
+}
+
+export default login
