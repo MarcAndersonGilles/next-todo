754a739862a36341a0b974f9018ced81d2df8eed
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-04-19 15:06:33 -0400

    installation du Auth, connection&SignOut, CLI

---
+++ components/SignupForm.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { createClient } from "@supabase/supabase-js";
+
+export default function SignupForm() {
+  
+  return (
+    <div>LoginPage</div>
+  )
+}
--- utils/supabaseClient.tsx
+++ utils/supabaseClient.tsx
@@ -1,2 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { createClient } from '@supabase/supabase-js'
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+import { Database } from '../types/supabase'
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+
+
+
+
+export const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const allOnlineUsers = await supabase.from('users').select('*').eq('status', 'ONLINE')
+    res.status(200).json(allOnlineUsers)
+  }
+  
+  
+  export default handler;
